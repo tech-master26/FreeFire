fa14326741819c8998b9f4aa1c8718ebc2f21dce
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-05-31 15:47:39 +0530

    Update logo2.sh

--- OnlineHacking/logo2.sh
+++ OnlineHacking/logo2.sh
@@ -38,7 +38,7 @@ echo ""
 echo -e "\e[1;96m< < < ============================================================ > > >\e[m "
 printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
 echo " "
-printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Game Kharido \e[0m  \e[95m[\e[93mPro\e[95m[\e[0m            \e[1;31m[\e[0m\e[1;92m06\e[0m\e[1;31m] \e[0m\e[1;91;107m Free Fire Skin \e[0m  \e[95m[\e[93mNew\e[95m[\e[0m "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Game Kharido \e[0m  \e[95m[\e[93mPro\e[95m[\e[0m     \e[1;31m[\e[0m\e[1;92m06\e[0m\e[1;31m] \e[0m\e[1;91;107m Free Fire Skin \e[0m  \e[95m[\e[93mNew\e[95m[\e[0m "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
 echo
@@ -46,15 +46,15 @@ printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Garena Gift Cent
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Magic Event Reward \e[0m               \e[1;31m[\e[0m\e[1;92m08\e[0m\e[1;31m] \e[0m\e[1;37;41m FreeFire Free Coin Diamond \e[0m  \e[95m[\e[93mNew\e[95m[\e[0m "
+printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Magic Event Reward \e[0m      \e[1;31m[\e[0m\e[1;92m08\e[0m\e[1;31m] \e[0m\e[1;37;41m FreeFire Free Coin Diamond \e[0m  \e[95m[\e[93mNew\e[95m[\e[0m "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Redemption Code \e[0m             \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m Fack WhatsApp Invite \e[0m  "
+printf "\e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Redemption Code \e[0m         \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m Fack WhatsApp Invite \e[0m  "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Diamond Buy \e[0m      \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Choose Gift + Package Gift + Lucky Spin \e[0m  "
+printf "\e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Diamond Buy \e[0m              \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Choose Gift + Package Gift + Lucky Spin \e[0m  "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"   
 echo
